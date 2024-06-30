3ae92d64d2763656c5b2810fb672db8d07f1f823
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-06-30 23:55:26 +0700

    Update copa.py

--- copa.py
+++ copa.py
@@ -24,13 +24,11 @@ def schedule_job_on_specific_date():
     wib = pytz.timezone('Asia/Jakarta')
     
     # Tanggal target
-    target_date = datetime(2024, 6, 29, tzinfo=wib)
+    target_date = datetime(2024, 7, 1, tzinfo=wib)
 
     # Daftar waktu target dalam satu hari
     target_times = [
-        "04:53",
-        "05:15",
-        "07:53",
+        "06:45",
         "08:15",
     ]
 
